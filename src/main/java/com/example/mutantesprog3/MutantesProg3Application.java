19baae6fd928c6b9fd31d2860e72d0392e45b26b
AgusMS7 <131214046+AgusMS7@users.noreply.github.com>
2024-10-11 15:31:54 -0300

    Add files via upload

---
+++ src/main/java/com/example/mutantesprog3/MutantesProg3Application.java
@@ -0,0 +1,13 @@
+package com.example.mutantesprog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantesProg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantesProg3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mutantesprog3/MutantesProg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutantesprog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantesProg3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
